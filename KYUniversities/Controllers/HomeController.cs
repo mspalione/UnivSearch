a488c7c7bdf223aa8224c027ba3e57faa599663c
Melissa Spalione <melissaspalione86@gmail.com>
2019-02-28 11:04:54 -0500

    trying to add a drop down menu for the state option

--- KYUniversities/Controllers/HomeController.cs
+++ KYUniversities/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ namespace KYUniversities.Controllers
 {
     public class HomeController : Controller
     {
-        
-
         public List<University> GetCSVData()
         {
             string path = Server.MapPath("~/Content/Univ.csv");
@@ -28,20 +26,17 @@ namespace KYUniversities.Controllers
                     var data = sCSVData[iCount].Split(',');
                     lstUniversity.Add(new University
                     {
-                        
-                    //Get relevant data from csv file
+                        //Get relevant data from csv file
                         InstName = data[1],
                         ChfName = data[8],
                         ChfTitle = data[9],
                         PhNum = data[10],
-                        WebAdd = data[1]
+                        WebAdd = data[15]
                     });
                 }
             }
-
             //return Json(lstUniversity, JsonRequestBehavior.AllowGet);
             return lstUniversity;
-
         }
 
         [HttpGet]
@@ -56,14 +51,41 @@ namespace KYUniversities.Controllers
         {
             var newItems = new List<string>();
             var path = Server.MapPath("~/Content/Univ.csv");
+            var states = GetAllStates();
+            var States = GetSelectListItems(states);
             this.ViewData["universities"] = GetCSVData();
 
             newItems.Add($" ,{vm.Institution},,,{vm.State},,,,{vm.Headname}, {vm.Headtitle}, {vm.Phone},,,,, {vm.Website}");
 
             System.IO.File.AppendAllLines(path, newItems);
-            System.Threading.Thread.Sleep(1000);
-            
+            //System.Threading.Thread.Sleep(1000);
+
             return View();
+        }
+
+        private IEnumerable<string> GetAllStates()
+        {
+            return new List<string>
+            {
+                "KY",
+            };
+        }
+
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<string> elements)
+        {
+            // Create an empty list to hold result of the operation
+            var selectList = new List<SelectListItem>();
+
+            foreach (var element in elements)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+
+            return selectList;
         }
     }
 }
